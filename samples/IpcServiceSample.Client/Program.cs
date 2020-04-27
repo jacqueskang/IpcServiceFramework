365a43b42b362148fa09b9da55281b7b34bdef7e
Jacques Kang <jkang.perso@gmail.com>
2020-04-27 11:42:36 +0200

    Refactor/graceful shutdown (#99)
    * refactor: update sample project
    
    * refactor: simplify IpcHostedService by inheriting from BackgroundService
    
    * refactor: fix failing build
    
    Co-authored-by: Jacques Kang <jacques.kang@voluntis.com>

---
+++ samples/IpcServiceSample.Client/Program.cs
@@ -0,0 +1,25 @@
+﻿using IpcServiceSample.ServiceContracts;
+using JKang.IpcServiceFramework.Client;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Threading.Tasks;
+
+namespace IpcServiceSample.ConsoleClient
+{
+    class Program
+    {
+        private static async Task Main(string[] args)
+        {
+            Console.WriteLine("Type a phrase and press enter:");
+            string input = Console.ReadLine();
+
+            IIpcClient<IInterProcessService> client = new ServiceCollection()
+                .AddNamedPipeIpcClient<IInterProcessService>("pipeinternal")
+                .BuildServiceProvider()
+                .GetRequiredService<IIpcClient<IInterProcessService>>();
+
+            string output = await client.InvokeAsync(x => x.ReverseString(input));
+            Console.WriteLine(output);
+        }
+    }
+}
---
+++ samples/IpcServiceSample.Server/InterProcessService.cs
@@ -0,0 +1,15 @@
+﻿using IpcServiceSample.ServiceContracts;
+using System;
+
+namespace IpcServiceSample.Server
+{
+    public class InterProcessService : IInterProcessService
+    {
+        public string ReverseString(string input)
+        {
+            char[] charArray = input.ToCharArray();
+            Array.Reverse(input.ToCharArray());
+            return new string(charArray);
+        }
+    }
+}
