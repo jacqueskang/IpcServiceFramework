439e1a017bc022b4cf5a8bef934cdb52e7e072ff
Jacques Kang <jacques.kang@voluntis.com>
2020-04-27 09:30:56 +0200

    refactor: update sample project

---
+++ samples/IpcServiceSample.Server/InterProcessService.cs
@@ -0,0 +1,13 @@
+﻿using IpcServiceSample.ServiceContracts;
+using System;
+
+namespace IpcServiceSample.Server
+{
+    public class InterProcessService : IInterProcessService
+    {
+        public string ReverseString(string input)
+        {
+            return new string(Array.Reverse(input.ToCharArray()));
+        }
+    }
+}
