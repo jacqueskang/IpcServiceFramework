7b57e0b84096898394abb5f7819d7ceda0996e4a
Jacques Kang <jacques.kang@voluntis.com>
2020-05-02 13:50:29 +0200

    refactor: provide tests for tcp client cancellation and timeout

---
+++ src/JKang.IpcServiceFramework.TcpTests/EdgeCaseTest.cs
@@ -0,0 +1,64 @@
+﻿using JKang.IpcServiceFramework.Client;
+using JKang.IpcServiceFramework.Testing.Fixtures;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace JKang.IpcServiceFramework.TcpTests
+{
+    public class EdgeCaseTest
+    {
+        [Fact]
+        public async Task ConnectionTimeout_Throw()
+        {
+            int timeout = 3000; // 3s
+            IIpcClient<ITestService> client = new ServiceCollection()
+                .AddTcpIpcClient<ITestService>(options =>
+                {
+                    // Connect to a non-routable IP address can trigger timeout
+                    options.ServerIp = IPAddress.Parse("10.0.0.0");
+                    options.ConnectionTimeout = timeout;
+                })
+                .BuildServiceProvider()
+                .GetRequiredService<IIpcClient<ITestService>>();
+
+            var sw = Stopwatch.StartNew();
+            await Assert.ThrowsAsync<TimeoutException>(async () =>
+            {
+                string output = await client.InvokeAsync(x => x.StringType("abc"));
+            });
+
+            Assert.True(sw.ElapsedMilliseconds < timeout * 2); // makesure timeout works with marge
+        }
+
+        [Fact]
+        public void ConnectionCancelled_Throw()
+        {
+            IIpcClient<ITestService> client = new ServiceCollection()
+                .AddTcpIpcClient<ITestService>(options =>
+                {
+                    // Connect to a non-routable IP address can trigger timeout
+                    options.ServerIp = IPAddress.Parse("10.0.0.0");
+                })
+                .BuildServiceProvider()
+                .GetRequiredService<IIpcClient<ITestService>>();
+
+            using var cts = new CancellationTokenSource();
+
+            Task.WaitAll(
+                Task.Run(async () =>
+                {
+                    await Assert.ThrowsAsync<OperationCanceledException>(async () =>
+                    {
+                        await client.InvokeAsync(x => x.ReturnVoid(), cts.Token);
+                    });
+                }),
+                Task.Run(() => cts.CancelAfter(1000)));
+        }
+    }
+}
