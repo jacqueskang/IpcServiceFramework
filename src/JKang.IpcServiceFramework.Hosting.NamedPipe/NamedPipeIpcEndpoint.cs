cd123a3644883898aebf9e5d8e238d83c3fff21b
Yi Ding <3415065+VaslD@users.noreply.github.com>
2020-05-22 12:19:03 +0800

    Set NamedPipe permissions to Everyone on Windows.

--- src/JKang.IpcServiceFramework.Hosting.NamedPipe/NamedPipeIpcEndpoint.cs
+++ src/JKang.IpcServiceFramework.Hosting.NamedPipe/NamedPipeIpcEndpoint.cs
@@ -1,9 +1,11 @@
-﻿using Microsoft.Extensions.Logging;
-using System;
+﻿using System;
 using System.IO;
 using System.IO.Pipes;
+using System.Runtime.InteropServices;
 using System.Threading;
 using System.Threading.Tasks;
+
+using Microsoft.Extensions.Logging;
 
 namespace JKang.IpcServiceFramework.Hosting.NamedPipe
 {
@@ -30,11 +32,30 @@ namespace JKang.IpcServiceFramework.Hosting.NamedPipe
                 throw new ArgumentNullException(nameof(process));
             }
 
-            using (var server = new NamedPipeServerStream(_options.PipeName, PipeDirection.InOut, _options.MaxConcurrentCalls,
-                PipeTransmissionMode.Byte, PipeOptions.Asynchronous))
+            // https://github.com/PowerShell/PowerShellEditorServices/blob/f45c6312a859cde4aa25ea347a345e1d35238350/src/PowerShellEditorServices.Protocol/MessageProtocol/Channel/NamedPipeServerListener.cs#L38-L67
+            // Unfortunately, .NET Core does not support passing in a PipeSecurity object into the constructor for
+            // NamedPipeServerStream so we are creating native Named Pipes and securing them using native APIs.
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
             {
-                await server.WaitForConnectionAsync(cancellationToken).ConfigureAwait(false);
-                await process(server, cancellationToken).ConfigureAwait(false);
+                PipeSecurity pipeSecurity = new PipeSecurity();
+                PipeAccessRule psRule = new PipeAccessRule(@"Everyone", PipeAccessRights.FullControl, System.Security.AccessControl.AccessControlType.Allow);
+                pipeSecurity.AddAccessRule(psRule);
+                using (var server = NamedPipeNative.CreateNamedPipe(_options.PipeName, (uint) _options.MaxConcurrentCalls, pipeSecurity))
+                {
+                    await server.WaitForConnectionAsync(cancellationToken).ConfigureAwait(false);
+                    await process(server, cancellationToken).ConfigureAwait(false);
+                }
+            }
+
+            // Use original logic on other platforms.
+            else
+            {
+                using (var server = new NamedPipeServerStream(_options.PipeName, PipeDirection.InOut, _options.MaxConcurrentCalls,
+                    PipeTransmissionMode.Byte, PipeOptions.Asynchronous))
+                {
+                    await server.WaitForConnectionAsync(cancellationToken).ConfigureAwait(false);
+                    await process(server, cancellationToken).ConfigureAwait(false);
+                }
             }
         }
     }
